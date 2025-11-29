e1038525d12fb0f05c84a3c76f5de3a15a9d1988
GenericDev <59420824+GenericDevv@users.noreply.github.com>
2025-11-29 17:09:35 +0000

    Test, New Project Structure

--- src/main/java/net/bigshot/armourprog/network/ModNetworking.java
+++ src/main/java/net/bigshot/armourprog/network/ModNetworking.java
@@ -2,33 +2,61 @@ package net.bigshot.armourprog.network;
 
 import net.bigshot.armourprog.ArmourProg;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
+
+import java.util.Optional;
 
 public class ModNetworking {
 
-    private static final String PROTOCOL_VERSION = "1";
+    private static final String VERSION = "1";
 
-    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
+    public static final net.minecraftforge.network.simple.SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
             new ResourceLocation(ArmourProg.MOD_ID, "main"),
-            () -> PROTOCOL_VERSION,
-            PROTOCOL_VERSION::equals,
-            PROTOCOL_VERSION::equals
+            () -> VERSION,
+            VERSION::equals,
+            VERSION::equals
     );
 
-    private static int packetId = 0;
-
-    private static int nextId() {
-        return packetId++;
-    }
+    private static int id = 0;
+    private static int next() { return id++; }
 
     public static void register() {
+
         CHANNEL.registerMessage(
-                nextId(),
+                next(),
                 SelectSewingRecipePacket.class,
                 SelectSewingRecipePacket::encode,
                 SelectSewingRecipePacket::decode,
-                SelectSewingRecipePacket::handle
+                SelectSewingRecipePacket::handle,
+                Optional.of(NetworkDirection.PLAY_TO_SERVER)
+        );
+
+        CHANNEL.registerMessage(
+                next(),
+                SyncLayersPacket.class,
+                SyncLayersPacket::encode,
+                SyncLayersPacket::new,
+                SyncLayersPacket::handle
+        );
+
+        CHANNEL.registerMessage(
+                next(),
+                SetLayerSlotPacket.class,
+                SetLayerSlotPacket::encode,
+                SetLayerSlotPacket::new,
+                SetLayerSlotPacket::handle
         );
     }
+
+    public static void sendTo(ServerPlayer player, Object msg) {
+        CHANNEL.send(PacketDistributor.PLAYER.with(() -> player), msg);
+    }
 }
+
+
+
+
